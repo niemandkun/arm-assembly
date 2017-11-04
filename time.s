ca0bc7d9396d8c399828f98cd140fa7b7600b09e
Poroshin Ivan <poroshin-ivan@yandex-team.ru>
2017-11-04 19:14:40 +0500

    + use mmap2 syscall to read RTC time

--- time.s
+++ time.s
@@ -1,51 +1,104 @@
-.global main
+.global _start
 
 .include "libprint.s"
 
 .text
 
-main:
-    push    {lr}
+_start:
+NITER           = 1
 
-    ldr     r0, =memfile
-    ldr     r1, =flags
-    mov     r7, #5
-    svc     #0
-
-    tst     r0, r0
-    blt     1f
-
-    mov     r11, r0
-    push    {r0}
-
-    bl      do_mmap
-    mov     r10, r0
-
-    pop     {r0}
-
-    ldr     r0, [r10, #16]
-    ldr     r1, =printbuf
-    bl      prntxbuf
-    prnts   printbuf, printbuf_len
-
-    ldr     r0, [r10, #20]
-    ldr     r1, =printbuf
-    bl      prntxbuf
-    prnts   printbuf, printbuf_len
-
-    mov     r0, r11
-    mov     r7, #6
-    svc     #0
-
+        bl      do_mmap_rtc_mem
+        ldr     r12, =NITER
 1:
-    pop     {pc}
+        bl      printtime
+        subs    r12, #1
+        bgt     1b
+
+        bl      do_close_rtc_mem
+
+exit:
+        eor     r0, r0
+        mov     r7, #1
+        svc     #0  @exit(0)
+
+
+do_mmap_rtc_mem:
+        # args: none
+        # returns: address of allocation start
+        push    {r1-r7,lr}
+
+O_RDWR          = 0x000002
+O_DSYNC         = 0x001000
+
+open_flags      = O_RDWR | O_DSYNC
+
+        ldr     r0, =memfile
+        ldr     r1, =open_flags
+        mov     r7, #5
+        svc     #0  @open(memfile)
+
+        tst     r0, r0
+        blt     exit
+
+        ldr     r1, =filedesc
+        str     r0, [r1]
+
+PROT_READ       = 0x01
+PROT_WRITE      = 0x02
+MAP_SHARED      = 0x01
+
+mmap_prot       = PROT_READ | PROT_WRITE
+mmap_flags      = MAP_SHARED
+rtc_base        = 0x00001f00
+
+        mov     r4, r0
+        eor     r0, r0
+        mov     r1, #1
+        ldr     r2, =mmap_prot
+        ldr     r3, =mmap_flags
+        ldr     r5, =rtc_base
+        mov     r7, #192
+        svc     #0  @mmap2(0, 1, mmap_prot, mmap_flags, 0, rtc_base_addr)
+
+        pop     {r1-r7,pc}
+
+
+do_close_rtc_mem:
+        push    {r0,r7,lr}
+        ldr     r0, =filedesc
+        ldr     r0, [r0]
+        mov     r7, #6
+        svc     #0  @close(r11)
+        pop     {r0,r7,pc}
+
+
+printtime:
+        # args: r0 - address of allocation start
+        push    {r0-r2,r7,r12,lr}
+
+        mov     r12, r0
+
+        ldr     r0, [r12, #16]
+        ldr     r1, =printbuf
+        bl      prntxbuf
+        prnts   printbuf, printbuf_len
+
+        ldr     r0, [r12, #20]
+        ldr     r1, =printbuf
+        bl      prntxbuf
+        prnts   printbuf, printbuf_len
+
+        pop     {r0-r2,r7,r12,pc}
+
+.bss
+
+filedesc:       .space 4
+
 
 .data
+printbuf:       .space 8
+                .ascii "\n"
 
-flags = 00000002 | 00010000
+printbuf_len    = . - printbuf
 
-memfile:    .asciz "/dev/mem"
-
-printbuf:   .space 8
-            .ascii "\n"
-printbuf_len = . - printbuf
+memfile:        .asciz "/dev/mem"
